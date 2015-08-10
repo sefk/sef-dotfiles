3dc6987d943d5bbc3779387ff9ac529adcdd3074
Sef Kloninger <sef@kloninger.com>
2015-08-10 19:06:12 +0000

    only source google cloud files if present

--- bash_startup/google-cloud.sh
+++ bash_startup/google-cloud.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
 
-# The next line updates PATH for the Google Cloud SDK.
-source '/Users/sef/src/google-cloud-sdk/path.bash.inc'
+GOOGLE_CLOUD_DIR=${HOME}/src/google-cloud-sdk
 
-# The next line enables bash completion for gcloud.
-source '/Users/sef/src/google-cloud-sdk/completion.bash.inc'
+if [ -e $GOOGLE_CLOUD_DIR ]; then
+    # The next line updates PATH for the Google Cloud SDK.
+    source '$GOOGLE_CLOUD_DIR/path.bash.inc'
+
+    # The next line enables bash completion for gcloud.
+    source '$GOOGLE_CLOUD_DIR/completion.bash.inc'
+fi
