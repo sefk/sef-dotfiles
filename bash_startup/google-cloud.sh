167234348b7c1eede097cc6e673533abf1ae1f87
Sef Kloninger <sef@kloninger.com>
2015-08-11 09:47:10 -0700

    fix quoting for google cloud setup

--- bash_startup/google-cloud.sh
+++ bash_startup/google-cloud.sh
@@ -4,8 +4,8 @@ GOOGLE_CLOUD_DIR=${HOME}/src/google-cloud-sdk
 
 if [ -e $GOOGLE_CLOUD_DIR ]; then
     # The next line updates PATH for the Google Cloud SDK.
-    source '$GOOGLE_CLOUD_DIR/path.bash.inc'
+    source $GOOGLE_CLOUD_DIR/path.bash.inc
 
     # The next line enables bash completion for gcloud.
-    source '$GOOGLE_CLOUD_DIR/completion.bash.inc'
+    source $GOOGLE_CLOUD_DIR/completion.bash.inc
 fi
