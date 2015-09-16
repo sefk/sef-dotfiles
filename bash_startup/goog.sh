a09c821a650b65355b291aff9369eae6b81505fc
Sef Kloninger <sef@kloninger.com>
2015-09-16 13:34:34 -0700

    add file for google environment stuff

---
+++ bash_startup/goog.sh
@@ -0,0 +1,9 @@
+export P4EDITOR=vim
+
+if ! shopt -oq posix; then
+    if [ -f /usr/share/bash-completion/bash_completion ]; then
+        . /usr/share/bash-completion/bash_completion
+    elif [ -f /etc/bash_completion ]; then
+       . /etc/bash_completion
+    fi
+fi
