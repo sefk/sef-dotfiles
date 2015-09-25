7f4060b8108fdadb969f1e815e2051220379eace
Sef Kloninger <sef@kloninger.com>
2015-09-25 14:27:31 -0700

    remove error when brew installed but no node

--- bash_startup/node.sh
+++ bash_startup/node.sh
@@ -1,6 +1,6 @@
 export NVM_DIR=~/.nvm
 
-if [ x`which brew` != x ]; then
+if [ x`which brew` != x ] && [[ -e $(brew --prefix nvm)/nvm.sh ]]; then
     source $(brew --prefix nvm)/nvm.sh
 else
     if [[ -e $NVM_DIR ]] && [[ -e $NVM_DIR/nvm.sh ]]; then
