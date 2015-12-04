4a6fa163d45497a6882a58b60eec45d84cbdfb83
Sef Kloninger <sef@kloninger.com>
2015-12-04 10:00:42 -0800

    unify history across buffers

--- bash_startup/history.sh
+++ bash_startup/history.sh
@@ -1,3 +1,5 @@
+export HISTCONTROL=ignoredups:erasedups
+export PROMPT_COMMAND="$PROMPT_COMMAND${PROMPT_COMMAND+;}history -a;history -c;history -r"
+export HISTSIZE=100000
+export HISTFILESIZE=100000
 shopt -s histappend
-export PROMPT_COMMAND="$PROMPT_COMMAND${PROMPT_COMMAND+;}history -a;history -n"
-export HISTFILESIZE=9999
