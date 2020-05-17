06e09fbe042e1faa26f4dc6b73d1f6d72616fe2a
Sef Kloninger <sefklon@gmail.com>
2020-05-17 11:05:49 -0700

    Remove more cruft from prior projects, mongo and mysql

--- mongorc.js
+++
@@ -1,9 +0,0 @@
-DBQuery.prototype._prettyShell = true;
-
-var colsizes = function() {
-    db.getCollectionNames().forEach(function(c) {
-        stat=db.getCollection(c).stats();
-        print(c, stat.size);
-    });
-};
-
